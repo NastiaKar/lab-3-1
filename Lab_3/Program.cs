6d7a98c2c7ae42ab1c2b51e0c8c3f1a3370f1b06
NastiaKar <nastia.karpyuk@outlook.com>
2022-05-10 16:05:43 +0300

    first commit

---
+++ Lab_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle testTruck = new Truck();
+            WriteLine(testTruck);
+            Vehicle testCar = new Car();
+            WriteLine(testCar);
+            Vehicle car1 = new Car("Toyota", "Green", 1400);
+            Vehicle car2 = new Car("Toyota", "Green", 1400);
+
+            WriteLine();
+            WriteLine($"Truck: {testTruck.GetHashCode()}\nCar: {testCar.GetHashCode()}");
+            WriteLine($"Car1 {car1.GetHashCode()}\nCar2: {car2.GetHashCode()}");
+            WriteLine();
+            WriteLine(car1.Equals(car2));
+
+            testTruck.ChangeLoadCapacity(12121212);
+            WriteLine(testTruck);
+            ReadKey();
+        }
+    }
+}
---
+++ Lab_3/Vehicle.cs
@@ -0,0 +1,96 @@
+﻿using static System.Console;
+
+namespace Lab_3;
+
+public abstract class Vehicle
+{
+    protected int _loadCapacity;
+    
+    public string Brand { get; protected set; }
+    public string Color { get; protected set; }
+    public virtual int LoadCapacity
+    {
+        protected set
+        {
+            if (value > 0) _loadCapacity = value;
+        }
+        get => _loadCapacity;
+    }
+
+    public Vehicle(string brand = "Mercedes", string color = "Black", int capacity = 1500) {
+        Brand = brand;
+        Color = color;
+        LoadCapacity = capacity;
+    }
+
+    public override string ToString() => $"Brand: {Brand}\nColor: {Color}\nCapacity: {LoadCapacity} kg";
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+        Vehicle? v = obj as Vehicle;
+        Write("Equality: ");
+        
+        return Brand == v!.Brand && Color == v.Color && LoadCapacity == v.LoadCapacity;
+    }
+
+    public override int GetHashCode()
+    {
+        Write("Hashcode: ");
+        return Brand.GetHashCode() + Color.GetHashCode() + LoadCapacity.GetHashCode();
+    }
+
+    public void ChangeBrand(string newBrand)
+    {
+        WriteLine($"Brand changed to {newBrand}");
+        Brand = newBrand;
+    }
+
+    public void ChangeColor(string newColor)
+    {
+        WriteLine($"Color changed to {newColor}");
+        Color = newColor;
+    }
+
+    public void ChangeLoadCapacity(int newCapacity)
+    {
+        LoadCapacity = newCapacity;
+        WriteLine($"Load capacity changed to {LoadCapacity} kg");
+    }
+}
+
+sealed class Truck : Vehicle {
+    public override int LoadCapacity
+    {
+        protected set
+        {
+            if (value < 10000) _loadCapacity = 10000;
+            else _loadCapacity = value;
+        }
+        get => _loadCapacity;
+    }
+
+    public Truck(string brand = "Ford", string color = "Red", int capacity = 10000) : base (brand, color)
+    {
+        LoadCapacity = capacity;
+    }
+}
+
+sealed class Car : Vehicle
+{
+    public override int LoadCapacity
+    {
+        protected set
+        {
+            if (value > 10000) _loadCapacity = 9000;
+            else _loadCapacity = value;
+        }
+        get => _loadCapacity;
+    }
+
+    public Car(string brand = "Toyota", string color = "Yellow", int capacity = 2000) : base(brand, color)
+    {
+        LoadCapacity = capacity;
+    }
+}
